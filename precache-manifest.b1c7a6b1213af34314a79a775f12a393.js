d9998dd348275e435c54b3eb788657c4d3597027
Vergil Choi <vergil.choi.zyc@gmail.com>
2019-05-03 14:45:29 +0800

    build 2019-05-03

---
+++ precache-manifest.b1c7a6b1213af34314a79a775f12a393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c483c59bc20f1a4bb0ce",
+    "url": "/static/css/main.3be2de2c.chunk.css"
+  },
+  {
+    "revision": "c483c59bc20f1a4bb0ce",
+    "url": "/static/js/main.c483c59b.chunk.js"
+  },
+  {
+    "revision": "c25768a54b8841922a40",
+    "url": "/static/js/1.c25768a5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "35381d90f565e8906feae9268de51ff3",
+    "url": "/index.html"
+  }
+];
